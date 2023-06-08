3d853b6d31b42d39193d2a2b149d42ab220cadf6
james mcleish <jamesmcleish@jamess-MacBook-Pro.local>
2023-06-08 12:58:01 +0100

    some mongo config and added to file structure

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,31 @@
 // index.js
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const port = 4000;
+const api = require
 
-// Define routes
+app.use(cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(api);
+
+module.exports = app;
+
+mongoose.connect('mongodb://localhost/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
