30e7956fa4d4283479d19256fe98f2b40cff79dc
james mcleish <jamesmcleish@jamess-MacBook-Pro.local>
2023-06-08 18:48:16 +0100

    set up home page of app

---
+++ Frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Type a number on the whiteboard and click enter.</p>
+        <p>[Whiteboard here]</p>
+        <button>Click Me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
